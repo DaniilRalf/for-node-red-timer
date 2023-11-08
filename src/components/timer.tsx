fa05997ab0efd1d2c065cd8c83a62e7b00cd55aa
DaniilRalf <ralf.danya@mail.ru>
2023-11-08 18:44:16 +0700

    add collapse block

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react'
+import {Collapse} from "antd";
 
+interface TimerConfInterface {
+    isOpen: boolean
+}
+
+/** timer-delay component */
 const Timer = () => {
+
+    const [timerConf, setTimerConf] = useState<TimerConfInterface>({} as TimerConfInterface)
+
+
+
     return (
         <div>
-
+            <Collapse
+                size="small"
+                expandIconPosition={'end'}
+                style={styleComponents.collapseBlock}
+                items={[{
+                    key: 'conf-default',
+                    // TODO: add translate
+                    label: 'Настройки',
+                    children:
+                        <p>asdasd</p>
+                }]}
+            />
+            <style>{styleAntdCustom}</style>
         </div>
     )
 }
 export default Timer
+
+
+/** styles for timer-delay component */
+const styleComponents = {
+    collapseBlock: {
+        width: '100%'
+    },
+    childBlock : {
+
+    }
+}
+/** styles for timer-delay ANTD library */
+const styleAntdCustom = `
+    .ant-collapse-header{display: flex; justify-content: end;}
+    .ant-collapse-header-text{flex: none;}
+`
